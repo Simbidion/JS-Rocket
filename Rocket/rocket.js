e000ac600744362af673977feb2808be587c2abf
Simbidion <simonadwyer@yahoo.com.au>
2017-05-11 14:40:51 +1000

    Linked up animation to countdown

--- Rocket/rocket.js
+++ Rocket/rocket.js
@@ -1,15 +1,40 @@
-var timer = null;
+
+
 var countdownNumber = 10;
 var state;
 
+var timer = null;
+
+
 var changeState = function (state) {
   document.body.className = "bodyState" + state;
+  clearInterval(timer);
+  countdownNumber = 10;
+  document.getElementById("countDown").innerHTML = countdownNumber;
 
-if (state == 2) {
-  timer = setInterval(function(){
-    document.getElementById("countDown").innerHTML = countdownNumber;
-    countdownNumber = countdownNumber-1;
-  }, 1000);
+//countdown
+  if (state == 2) {
+    timer=setInterval(function(){
+      countdownNumber = countdownNumber-1;
+      document.getElementById("countDown").innerHTML = countdownNumber;
+      if (countdownNumber<=0) {
+        changeState(3);
+      };
+    }, 500);
+    }
 
-}
+//decide if success or fail
+  else if (state == 3) {
+    var success = setTimeout(function() {
+      var randomNumber = Math.round(Math.random() *10);
+      console.log("Random number: "  +randomNumber);
+      if (randomNumber<5) {
+        changeState(5);
+      }
+      else {
+        changeState(4);
+      }
+    }, 500);
+
+  }
 }
